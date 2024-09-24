8a9f31547a29c13841cd41e076ff28e83dd80649
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-24 13:22:27 -0500

    Recuprar informacion de un post

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,12 +2,23 @@ import { Router } from "express";
 
 export const authRouter = Router();
 
-authRouter.post("/", (req, res) => {});
+authRouter.post("/", (req, res) => {
+  const { email, password } = req.body;
+
+  res.send({
+    ok: true,
+    message: "login route",
+    email,
+    password,
+  });
+});
 
 authRouter.post("/register", (req, res) => {
   res.send({
     ok: true,
     message: "register route",
+    name,
+    email,
   });
 });
 
