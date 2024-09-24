33bd7e33815eaa0209c8367209c7591aa7975e3b
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-24 13:22:04 -0500

    Refactor app.js to add middleware for JSON parsing and move static directory middleware

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ import routes from "./routes.js";
 
 const app = express();
 
-// Public directory
-app.use(express.static("public"));
+// Middlewares
+app.use(express.json());
 
 // Routes
 app.use("/api/v1", routes);
+
+// Public directory
+app.use(express.static("public"));
 
 export default app;
