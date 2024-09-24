92a12cfd39c5bb5277ba4f518e6600464d7b13b3
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-24 17:55:57 -0500

    Add cors dependency and enable CORS middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import routes from "./routes.js";
+import cors from "cors";
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/v1", routes);
