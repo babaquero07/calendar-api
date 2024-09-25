65305c25d718ec3e892ff59d2e68a8cd5ac392d9
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2024-09-25 18:24:47 -0500

    fix auth validator

--- src/auth/middlewares/auth-validator.js
+++ src/auth/middlewares/auth-validator.js
@@ -1,12 +1,11 @@
 import { body } from "express-validator";
 
 export const registerValidator = [
-  body("name", "Name is required")
-    .not()
-    .isEmpty()
-    .withMessage("Name is required"),
-  body("email", "Email is required").isEmail().withMessage("Email is invalid"),
-  body("password", "Password is required").isLength({ min: 8 }),
+  body("name", "Name is required").not().isEmpty(),
+  body("email").isEmail().withMessage("Email is invalid"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
 ];
 
 export const loginValidator = [
